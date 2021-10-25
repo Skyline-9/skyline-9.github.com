8e8f5cfeb00bda700e310e158609a44d2c4ede80
Skyline-9 <luo.richard@gmail.com>
2021-10-24 21:09:29 -0400

    🍱 Updated resume

--- src/App.js
+++ src/App.js
@@ -26,261 +26,262 @@ import CodeIcon from "@material-ui/icons/Code";
 import "./App.css";
 
 //React hooks
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 
 /* ==== Navigation Bar ==== */
 //Navigation bar icon
-const navBrand = <img src={Logo} alt="Logo" height="40px"></img>;
+const navBrand = <img src={Logo} alt="Logo" height="40px"/>;
 
 //Navigation bar links
 const leftLinks = (
-	<>
-		<a href="#header">
-			<HomeIcon className="icon" /> Home
-		</a>
-		<a href="#CS">
-			<DesktopWindowsIcon className="icon" /> Computer Science
-		</a>
-		<a href="#Education">
-			<SchoolIcon className="icon" /> Education
-		</a>
-	</>
+    <>
+        <a href="#header">
+            <HomeIcon className="icon"/> Home
+        </a>
+        <a href="#CS">
+            <DesktopWindowsIcon className="icon"/> Computer Science
+        </a>
+        <a href="#Education">
+            <SchoolIcon className="icon"/> Education
+        </a>
+    </>
 );
 
 //Typewriter
 const headerTypewriter = (
-	<ReactTypingEffect
-		className="typewriter"
-		text={[
-			"Salutations, I am Richard, an undergraduate student researcher at Georgia Tech College of Computing.",
-			"Welcome to my personal website.",
-			"I hope you find something interesting here!",
-		]}
-		speed="90"
-		eraseSpeed="20"
-	/>
+    <ReactTypingEffect
+        className="typewriter"
+        text={[
+            "Salutations, I am Richard, an undergraduate student researcher at Georgia Tech College of Computing.",
+            "Welcome to my personal website.",
+            "I hope you find something interesting here!",
+        ]}
+        speed="90"
+        eraseSpeed="20"
+    />
 );
 
 const educationTypewriter = (
-	<ReactTypingEffect className="typewriter" text={["Go Jackets!"]} speed="90" eraseSpeed="20" />
+    <ReactTypingEffect className="typewriter" text={["Go Jackets!"]} speed="90" eraseSpeed="20"/>
 );
 
 /* ==== useWindowSize hook for finding viewport dimensions */
 function useWindowSize() {
-	// Initialize state with undefined width/height so server and client renders match
-	const [windowSize, setWindowSize] = useState({
-		width: undefined,
-		height: undefined,
-	});
-	useEffect(() => {
-		// Handler to call on window resize
-		function handleResize() {
-			// Set window width/height to state
-			setWindowSize({
-				width: window.innerWidth,
-				height: window.innerHeight,
-			});
-		}
-		// Add event listener
-		window.addEventListener("resize", handleResize);
-		// Call handler right away so state gets updated with initial window size
-		handleResize();
-		// Remove event listener on cleanup
-		return () => window.removeEventListener("resize", handleResize);
-	}, []); // Empty array ensures that effect is only run on mount
-	return windowSize;
+    // Initialize state with undefined width/height so server and client renders match
+    const [windowSize, setWindowSize] = useState({
+        width: undefined,
+        height: undefined,
+    });
+    useEffect(() => {
+        // Handler to call on window resize
+        function handleResize() {
+            // Set window width/height to state
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        }
+
+        // Add event listener
+        window.addEventListener("resize", handleResize);
+        // Call handler right away so state gets updated with initial window size
+        handleResize();
+        // Remove event listener on cleanup
+        return () => window.removeEventListener("resize", handleResize);
+    }, []); // Empty array ensures that effect is only run on mount
+    return windowSize;
 }
 
 /* ==== Function App ==== */
 function App() {
-	const viewportSize = useWindowSize();
+    const viewportSize = useWindowSize();
 
-	const params = {
-		particles: {
-			number: {
-				value: viewportSize.width / 20,
-			},
-			size: {
-				value: 3,
-			},
-		},
-		interactivity: {
-			events: {
-				onhover: {
-					enable: true,
-					mode: "repulse",
-				},
-			},
-		},
-	};
+    const params = {
+        particles: {
+            number: {
+                value: viewportSize.width / 20,
+            },
+            size: {
+                value: 3,
+            },
+        },
+        interactivity: {
+            events: {
+                onhover: {
+                    enable: true,
+                    mode: "repulse",
+                },
+            },
+        },
+    };
 
-	return (
-		<div>
-			<Navbar brand={navBrand} leftLinks={leftLinks} shouldAnimate={true} shouldHideOnScroll={false} />
-			<Header
-				caption="Richard Luo's Website"
-				subtitle="Making Dreams Come True Since 2002"
-				backgroundImage={bg}
-				id="header"
-			/>
-			<HighlightCard
-				typewriter={headerTypewriter}
-				title="About Me"
-				content={
-					<p>
-						👋 Hi there! I’m Richard, an undergraduate student researcher at Georgia Tech's College of
-						Computing with a broad range of interests. Currently, I'm pursuing a B.S. in Computer Science
-						from Georgia Tech, and at GT, I'm part of The Agency (ML Club), GreyHat (Cybersecurity Club),
-						and CSA. If you're interested in working with me, feel free to shoot me a message!
-					</p>
-				}
-				alt="Picture of Me"
-				imageSrc={AboutMe}
-			/>
+    return (
+        <div>
+            <Navbar brand={navBrand} leftLinks={leftLinks} shouldAnimate={true} shouldHideOnScroll={false}/>
+            <Header
+                caption="Richard Luo's Website"
+                subtitle="Making Dreams Come True Since 2002"
+                backgroundImage={bg}
+                id="header"
+            />
+            <HighlightCard
+                typewriter={headerTypewriter}
+                title="About Me"
+                content={
+                    <p>
+                        👋 Hi there! I’m Richard, an undergraduate student researcher at Georgia Tech's College of
+                        Computing with a broad range of interests. Currently, I'm pursuing a B.S. in Computer Science
+                        from Georgia Tech, and at GT, I'm part of The Agency (ML Club), GreyHat (Cybersecurity Club),
+                        and CSA. If you're interested in working with me, feel free to shoot me a message!
+                    </p>
+                }
+                alt="Picture of Me"
+                imageSrc={AboutMe}
+            />
 
-			<button
-				className="scifi-button"
-				onClick={() => {
-					window.open(resume);
-				}}
-			>
-				Check Out My Resume
-			</button>
+            <button
+                className="scifi-button"
+                onClick={() => {
+                    window.open(resume);
+                }}
+            >
+                Check Out My Resume
+            </button>
 
-			<div id="social-media-container">
-				<a href="https://www.linkedin.com/in/richardluorl" target="_blank" rel="noopener noreferrer nofollow">
-					<LinkIcon className="icon" /> LinkedIn
-				</a>
-				<a href="mailto:luo.richard@gmail.com" target="_blank" rel="noopener noreferrer nofollow">
-					<EmailIcon className="icon" /> Email
-				</a>
-				<a href="https://github.com/Skyline-9" target="_blank" rel="noopener noreferrer nofollow">
-					<CodeIcon className="icon" /> GitHub
-				</a>
-			</div>
-			<br />
-			<CustomParticles id="CS" params={params} title="Computer Science" />
-			<HighlightCard
-				title="Experience"
-				content={
-					<div id="CS-Card">
-						<b>Cybersecurity Analyst</b>
-						<br />
-						<p>
-							<i>Georgia Institute of Technology</i>
-						</p>
-						<p>
-							<i>June 2021 - August 2021</i>
-						</p>
-						<br />
-						<p>
-							Identified and mitigated security incidents with FireEye and Palo Alto Networks Cortex
-							XSOAR, triaged incoming threats, and managed network traffic. Reviewed compromised systems
-							and worked alongside system administrators to appropriately mitigate any active threats
-						</p>
-					</div>
-				}
-				alt="Picture of Me"
-				imageSrc={CSImage}
-			/>
+            <div id="social-media-container">
+                <a href="https://www.linkedin.com/in/richardluorl" target="_blank" rel="noopener noreferrer nofollow">
+                    <LinkIcon className="icon"/> LinkedIn
+                </a>
+                <a href="mailto:luo.richard@gmail.com" target="_blank" rel="noopener noreferrer nofollow">
+                    <EmailIcon className="icon"/> Email
+                </a>
+                <a href="https://github.com/Skyline-9" target="_blank" rel="noopener noreferrer nofollow">
+                    <CodeIcon className="icon"/> GitHub
+                </a>
+            </div>
+            <br/>
+            <CustomParticles id="CS" params={params} title="Computer Science"/>
+            <HighlightCard
+                title="Experience"
+                content={
+                    <div id="CS-Card">
+                        <b>Cybersecurity Analyst</b>
+                        <br/>
+                        <p>
+                            <i>Georgia Institute of Technology</i>
+                        </p>
+                        <p>
+                            <i>June 2021 - August 2021</i>
+                        </p>
+                        <br/>
+                        <p>
+                            Identified and mitigated security incidents with FireEye and Palo Alto Networks Cortex
+                            XSOAR, triaged incoming threats, and managed network traffic. Reviewed compromised systems
+                            and worked alongside system administrators to appropriately mitigate any active threats
+                        </p>
+                    </div>
+                }
+                alt="Picture of Me"
+                imageSrc={CSImage}
+            />
 
-			<h2>🥼 Undergraduate Research</h2>
-			<p>
-				<b>
-					<i>Robot Autonomy and Interactive Learning (RAIL) Lab @ GT</i>
-				</b>
-				<br />
-				<i>August 2021 - Current</i>
-				<br />
-			</p>
-			<p className="extra-text">
-				Working with Dr. Chernova in Robot Autonomy & Interactive Learning (RAIL) Lab @ GT on smart home active
-				assistance
-			</p>
-			<p>
-				<b>
-					<i>Georgia Institute of Technology College of Computing</i>
-				</b>
-				<br />
-				<i>January 2021 - August 2021</i>
-				<br />
-			</p>
-			<p className="extra-text">
-				Researching privacy-preserving biometric under the supervision of Dr. Wenke Lee and Erkam Uzun. Built
-				applications with React.js/Tensorflow.js for remote biometric authentication using deep learning
-				recognition schemes to bridge deep learning inferences with standard privacy-preserving primitives like
-				fuzzy extractors.
-			</p>
+            <h2>🥼 Undergraduate Research</h2>
+            <p>
+                <b>
+                    <i>Robot Autonomy and Interactive Learning (RAIL) Lab @ GT</i>
+                </b>
+                <br/>
+                <i>August 2021 - Current</i>
+                <br/>
+            </p>
+            <p className="extra-text">
+                Working with Dr. Chernova in Robot Autonomy & Interactive Learning (RAIL) Lab @ GT on smart home active
+                assistance
+            </p>
+            <p>
+                <b>
+                    <i>Georgia Institute of Technology College of Computing</i>
+                </b>
+                <br/>
+                <i>January 2021 - August 2021</i>
+                <br/>
+            </p>
+            <p className="extra-text">
+                Researching privacy-preserving biometric under the supervision of Dr. Wenke Lee and Erkam Uzun. Built
+                applications with React.js/Tensorflow.js for remote biometric authentication using deep learning
+                recognition schemes to bridge deep learning inferences with standard privacy-preserving primitives like
+                fuzzy extractors.
+            </p>
 
-			<h2>Achievements</h2>
-			<p className="extra-text">
-				CSAW CTF Finalist Qualifier
-				<br /> CUCTF 2nd Place <br />
-				BSidesBosCTF Top 1% Worldwide <br />
-				Stanford Programming Contest (ProCo) 1st Place <br />
-				CMU picoCTF - Top 3.5% International <br />
-				United States of America Computing Olympiad (USACO) - Gold <br />
-				Harker Programming Invitational 3rd Place
-			</p>
+            <h2>Achievements</h2>
+            <p className="extra-text">
+                CSAW CTF Finalist Qualifier
+                <br/> CUCTF 2nd Place <br/>
+                BSidesBosCTF Top 1% Worldwide <br/>
+                Stanford Programming Contest (ProCo) 1st Place <br/>
+                CMU picoCTF - Top 3.5% International <br/>
+                United States of America Computing Olympiad (USACO) - Gold <br/>
+                Harker Programming Invitational 3rd Place
+            </p>
 
-			<br />
-			<button
-				className="scifi-button"
-				onClick={() => {
-					window.location.href = "https://github.com/Skyline-9";
-				}}
-			>
-				Check Out My Projects
-			</button>
+            <br/>
+            <button
+                className="scifi-button"
+                onClick={() => {
+                    window.location.href = "https://github.com/Skyline-9";
+                }}
+            >
+                Check Out My Projects
+            </button>
 
-			<br />
+            <br/>
 
-			<Header caption="Education" backgroundImage={Education} id="Education" />
-			<HighlightCard
-				title="Education"
-				typewriter={educationTypewriter}
-				content={
-					<div id="CS-Card">
-						<b>🐝 Georgia Institute of Technology</b>
-						<br />
-						<p>
-							<i>Computer Science Major | GPA: 4.0</i>
-						</p>
-						<p>
-							<i>Threads: Intelligence/Info & Internetworks</i>
-						</p>
-						<p>
-							<i>August 2020 - Present</i>
-						</p>
-						<br />
-						<b>Relevant Courses</b>
-						<p>
-							Linear Algebra
-							<br />
-							Introduction to Discrete Mathematics
-							<br />
-							Multivaraible Calculus
-							<br />
-							Intro to OOP
-							<br />
-							Intro to Data Structures and Algorithms
-							<br />
-							Combinatorics
-							<br />
-							Computer Organization and Programming
-							<br />
-							Objects and Design
-							<br />
-							Intro to AI
-							<br />
-						</p>
-					</div>
-				}
-				alt="Picture of Me"
-				imageSrc={GT}
-			/>
-		</div>
-	);
+            <Header caption="Education" backgroundImage={Education} id="Education"/>
+            <HighlightCard
+                title="Education"
+                typewriter={educationTypewriter}
+                content={
+                    <div id="CS-Card">
+                        <b>🐝 Georgia Institute of Technology</b>
+                        <br/>
+                        <p>
+                            <i>Computer Science Major | GPA: 4.0</i>
+                        </p>
+                        <p>
+                            <i>Threads: Intelligence/Info & Internetworks</i>
+                        </p>
+                        <p>
+                            <i>August 2020 - Present</i>
+                        </p>
+                        <br/>
+                        <b>Relevant Courses</b>
+                        <p>
+                            Linear Algebra
+                            <br/>
+                            Introduction to Discrete Mathematics
+                            <br/>
+                            Multivaraible Calculus
+                            <br/>
+                            Intro to OOP
+                            <br/>
+                            Intro to Data Structures and Algorithms
+                            <br/>
+                            Combinatorics
+                            <br/>
+                            Computer Organization and Programming
+                            <br/>
+                            Objects and Design
+                            <br/>
+                            Intro to AI
+                            <br/>
+                        </p>
+                    </div>
+                }
+                alt="Picture of Me"
+                imageSrc={GT}
+            />
+        </div>
+    );
 }
 
 export default App;

2b0ce15e8cfadfea1312d3d56788ac53f9cef2bc
Skyline-9 <luo.richard@gmail.com>
2022-08-03 09:40:02 -0700

    :sparkles: Added UKG experience

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,20 +1,37 @@
-import './Experience.css'
-import '../../Data.jsx'
-import gatech from '../../resources/images/experiences/gatech.png'
-import rail from '../../resources/images/experiences/rail.png'
+import './Experience.css';
+import '../../Data.jsx';
+import gatech from '../../resources/images/experiences/gatech.png';
+import rail from '../../resources/images/experiences/rail.png';
+import ukg from '../../resources/images/experiences/ukg.jpeg';
 
 export default function Experience() {
     return (
-        <div className="experience" id='experience'>
+        <div className="experience" id="experience">
             <h1>Experience</h1>
             <div className="experience-card">
                 <div className="experience-image">
-                    <img src={rail} alt="experience-1"/>
+                    <img src={ukg} alt="experience-1" />
                 </div>
                 <div className="experience-text">
-                    <b>Research Assistant</b><br/>
-                    <b>Georgia Tech RAIL</b><br/>
-                    <i>August 2021 - Present</i><br/>
+                    <b>Software Engineering Intern</b><br />
+                    <b>Ultimate Kronos Group</b><br />
+                    <i>May 2022 - August 2022</i><br />
+                    <p>
+                        Developed new features as part of Workforce TeleStaff New Product Development Team in Agile lifecycle.
+                        Created Angular proof-of-concept for TeleStaff, which improves UI/UX, increases responsiveness, and meets WCAG 2.1 Accessability guidelines.
+                        Established reverse proxy to make calls with existing backend and support legacy code.
+                        Presented proof-of-concept to multiple VPs and directors to discuss future company outlooks and product roadmaps Implemented REST APIs using Java Spring Services to support CRUD operations for MSSQL database.
+                    </p>
+                </div>
+            </div>
+            <div className="experience-card">
+                <div className="experience-image">
+                    <img src={rail} alt="experience-1" />
+                </div>
+                <div className="experience-text">
+                    <b>Research Assistant</b><br />
+                    <b>Georgia Tech RAIL</b><br />
+                    <i>August 2021 - Present</i><br />
                     <p>
                         Working with Dr. Chernova in Robot Autonomy & Interactive Learning (RAIL) Lab on smart-home
                         active assistance based on deep-learning sensor prediction
@@ -23,12 +40,12 @@ export default function Experience() {
             </div>
             <div className="experience-card">
                 <div className="experience-image">
-                    <img src={gatech} alt="experience-2"/>
+                    <img src={gatech} alt="experience-2" />
                 </div>
                 <div className="experience-text">
-                    <b>Cybersecurity Analyst</b><br/>
-                    <b>Georgia Tech Security Operations Center</b><br/>
-                    <i>June 2021 - August 2021</i><br/>
+                    <b>Cybersecurity Analyst</b><br />
+                    <b>Georgia Tech Security Operations Center</b><br />
+                    <i>June 2021 - August 2021</i><br />
                     <p>
                         Identified and mitigated security incidents with FireEye and Palo Alto Networks Cortex XSOAR,
                         triaged incoming threats, and managed network traffic. Reviewed compromised systems and worked
@@ -38,12 +55,12 @@ export default function Experience() {
             </div>
             <div className="experience-card">
                 <div className="experience-image">
-                    <img src={gatech} alt="experience-3"/>
+                    <img src={gatech} alt="experience-3" />
                 </div>
                 <div className="experience-text">
-                    <b>Undergraduate Researcher</b><br/>
-                    <b>Georgia Tech Research Institute</b><br/>
-                    <i>January 2021 - August 2021</i><br/>
+                    <b>Undergraduate Researcher</b><br />
+                    <b>Georgia Tech Research Institute</b><br />
+                    <i>January 2021 - August 2021</i><br />
                     <p>
                         Researched privacy-preserving biometric under the supervision of Dr. Wenke Lee. Built
                         applications using React.js/Tensorflow.js with remote biometric authentication using deep

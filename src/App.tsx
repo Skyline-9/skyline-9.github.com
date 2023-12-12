e36662129a3161af17706f494176c6ab6e8353ae
Richard Luo <luo.richard@gmail.com>
2023-12-11 23:25:31 -0500

    :bug: Temp remove sakana

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,11 @@ import "@fontsource/nunito-sans"; // Defaults to weight 400.
 import './App.css';
 
 // Components
-// import CyberpunkGlitch from "../../Downloads/skyline-9.github.com-main/src/components/CyberpunkGlitch";
-// import ExperienceTimeline from "../../Downloads/skyline-9.github.com-main/src/components/ExperienceTimeline";
-// import IntroCard from "../../Downloads/skyline-9.github.com-main/src/components/IntroCard";
-// import Projects from "../../Downloads/skyline-9.github.com-main/src/components/Projects";
-// import SakanaReact from 'sakana-react';
-
 import CyberpunkGlitch from "./components/CyberpunkGlitch";
 import ExperienceTimeline from "./components/ExperienceTimeline";
 import IntroCard from "./components/IntroCard";
 import Projects from "./components/Projects";
-import SakanaReact from 'sakana-react';
+// import SakanaReact from 'sakana-react';
 
 const App = () => {
 
@@ -24,11 +18,11 @@ const App = () => {
             <IntroCard />
             <ExperienceTimeline/>
             <Projects />
-            <div className="sakana">
-                <SakanaReact className="sakana"
-                             showControler={false}/>
-                <p className="sakana-subtext"><i>Drag me around!</i></p>
-            </div>
+            {/*<div className="sakana">*/}
+            {/*    <SakanaReact className="sakana"*/}
+            {/*                 showControler={false}/>*/}
+            {/*    <p className="sakana-subtext"><i>Drag me around!</i></p>*/}
+            {/*</div>*/}
         </div>
     );
 }

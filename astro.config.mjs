bde2257b765ed83707a2467cf0c0b69656c7deae
Richard Luo <luo.richard@gmail.com>
2026-01-15 19:12:19 -0800

    Feat: update metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://skyline-9.github.io",
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), sitemap()],
 });
